ea9f8049559ff6eeae4758262f294d3a9847646e
waseembaloch21 <154684340+waseembaloch21@users.noreply.github.com>
2024-11-30 10:46:28 +0500

    first commit

---
+++ python.py
@@ -0,0 +1,16 @@
+print('hello world')
+
+vars = 'saif'
+vars1 = ['apple', 'orange', 'banana']
+
+print(vars)
+print(vars1)
+
+if(vars1 != 'apple') :print('good')
+
+else: print('not good')
+
+
+print('hello Phyton')
+
+
