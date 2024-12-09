3f0fb209573255f9be2ae0459ed38a55b85e8ab8
waseembaloch21 <154684340+waseembaloch21@users.noreply.github.com>
2024-12-09 15:34:34 +0500

    code done

--- python.py
+++ python.py
@@ -14,6 +14,12 @@ else: print('not good')
 
 print('hello Phyton')
 
-
+# Generate a random number
 print(f"Random num : {random.randint(1, 100)}")
 
+# Convert celsius to fahrenheit
+
+celsius = float(input("Enter temperature in celsius :"))
+
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} degress celsius is equal to {fahrenheit} degree fahrenheit")
